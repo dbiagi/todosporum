cbb36ee17260a5b9e40df82e2e2251ab2b55e51c
Diego de Biagi <diegobiagiviana@gmail.com>
2016-09-21 00:33:43 -0300

    Adicionado tela do editor de texto.

---
+++ src/DBiagi/MainBundle/Controller/AnimappController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DBiagi\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AnimappController extends Controller {
+    
+    /**
+     * @Route("/animapp", name="main_animapp")
+     */
+    public function editorAction(){
+        return $this->render('Animapp/animapp.html.twig');
+    }
+}
