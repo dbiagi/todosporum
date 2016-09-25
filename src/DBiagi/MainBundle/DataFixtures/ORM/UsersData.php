6f7f7bf0fda2b5724a773a34f4ae9b910f36a6c7
Diego de Biagi <diegobiagiviana@gmail.com>
2016-09-25 20:16:31 -0300

    Adicionado fixtures para criar usuários.

---
+++ src/DBiagi/MainBundle/DataFixtures/ORM/UsersData.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace DBiagi\MainBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use DBiagi\MainBundle\Entity\User;
+
+/**
+ * Description of UsersData
+ *
+ * @author diego
+ */
+class UsersData extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface {
+
+    /** @var ContainerInterface */
+    private $container = null;
+
+    /** @var \Faker\Generator */
+    private $faker = null;
+
+    const USERS_COUNT = 30;
+
+    public function load(ObjectManager $manager) {
+        $this->faker = $this->container->get('utils.faker');
+
+        $this->loadAdmin($manager);
+        $this->loadUsers($manager);
+    }
+
+    private function loadUsers(ObjectManager $manager) {
+        for ($i = 0; $i < static::USERS_COUNT; $i++) {
+            $email = $this->faker->email;
+            
+            $user = new User();
+            $user
+                ->setFirstName($this->faker->firstName)
+                ->setLastName($this->faker->lastName)
+                ->setEmail($email)
+                ->setUsername($email)
+                ->setPlainPassword($this->faker->password)
+                ->setEnabled(true)
+            ;
+            
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+
+    private function loadAdmin(ObjectManager $manager) {
+        $admin = new User();
+        $admin->setFirstName('Admin')
+            ->setEmail($this->container->getParameter('admin_email'))
+            ->setUsername($this->container->getParameter('admin_email'))
+            ->setPlainPassword('admin')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setEnabled(true)
+        ;
+
+        $manager->persist($admin);
+        $manager->flush();
+
+        $this->setReference('admin', $admin);
+    }
+
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+
+    public function getOrder() {
+        return 1;
+    }
+
+}
