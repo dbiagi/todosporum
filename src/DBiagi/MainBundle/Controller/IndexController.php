647e4d6b4437fcda79d2ba5ad4e7d3d0ca9199d2
Diego de Biagi <diegobiagiviana@gmail.com>
2017-02-11 17:39:13 -0200

    Removido execção da página inicial.

--- src/DBiagi/MainBundle/Controller/IndexController.php
+++ src/DBiagi/MainBundle/Controller/IndexController.php
@@ -4,42 +4,39 @@ namespace DBiagi\MainBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends Controller {
 
-	/**
-	 * @Route("/", name="main_home")
-	 * @return Response
-	 */
-	public function indexAction(){
-
-	    throw new \Exception('Exceção aleatória');
-		return $this->render('Pages/home.html.twig');
-	}
-
-	/**
-	 * @Route("projeto", name="main_projeto")
-	 * @return Response
-	 */
-    public function projetoAction(){
-		return $this->render('Pages/projeto.html.twig');
+    /**
+     * @Route("/", name="main_home")
+     * @return Response
+     */
+    public function indexAction() {
+        return $this->render('Pages/home.html.twig');
     }
 
-	/**
-	 * @Route("/politica-de-privacidade-e-copyright", name="main_politica")
-	 * @return Response
-	 */
-    public function politicaAction(){
-    	return $this->render('Pages/termo.html.twig');
+    /**
+     * @Route("projeto", name="main_projeto")
+     * @return Response
+     */
+    public function projetoAction() {
+        return $this->render('Pages/projeto.html.twig');
     }
 
-	/**
-	 * @Route("/termo", name="main_termo")
-	 * @return Response
-	 */
-    public function termoAction(){
-    	return $this->render('Pages/termo.html.twig');
+    /**
+     * @Route("/politica-de-privacidade-e-copyright", name="main_politica")
+     * @return Response
+     */
+    public function politicaAction() {
+        return $this->render('Pages/termo.html.twig');
+    }
+
+    /**
+     * @Route("/termo", name="main_termo")
+     * @return Response
+     */
+    public function termoAction() {
+        return $this->render('Pages/termo.html.twig');
     }
 }
